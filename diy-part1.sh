dd543471bf3750d7ec2956824edec4295912143c
rfuger <68551351+rfuger@users.noreply.github.com>
2020-08-26 18:38:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Lienol/openwrt-package
-sed -i '$a src-git lienol https://github.com/TinyTitanPro/lienol-openwrt-package.git' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #=================================================
 # Description: DIY script
@@ -39,7 +38,7 @@ mkdir package/community
 pushd package/community
 
 # Add Lienol's Packages
-# git clone --depth=1 https://github.com/SuLingGG/openwrt-package
+git clone --depth=1 https://github.com/Lienol/openwrt-package
 
 # Add mentohust & luci-app-mentohust.
 # git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
